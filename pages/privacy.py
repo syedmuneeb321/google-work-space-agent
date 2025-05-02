b02b7df1dc27bfd7ef94304a3d85171fde79f2d6
Syed Muneeb Abbas <130203424+syedmuneeb321@users.noreply.github.com>
2025-05-02 12:28:59 +0500

    Create privacy.py

---
+++ pages/privacy.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Privacy Policy")
+st.write("We use Supabase to store your login data safely. We use Google OAuth to connect you to Google Workspace. We don’t share your data with others. We keep it secure.")
+st.write("If you have questions, email us at asadsher2324@gmail.com.")
