42295d66bb61084a00d98335a9bc2c980c55377d
Syed Muneeb Abbas <130203424+syedmuneeb321@users.noreply.github.com>
2025-05-02 12:29:41 +0500

    Create terms.py

---
+++ pages/terms.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Terms of Service")
+st.write("You can use this app to manage Google Workspace tasks. Don’t use it for bad things. Follow Google’s rules. We can stop your access if you break these rules.")
+st.write("Contact us at asadsher2324@gmail.com for help.")
